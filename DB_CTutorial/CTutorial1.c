b5113ade41056ce065bc0151b93d1bb7dc1a224a
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-19 03:19:49 -0500

    Create CTutorial1.c

---
+++ DB_CTutorial/CTutorial1.c
@@ -0,0 +1,29 @@
+/* Multiline                                                                                                       
+Comment */
+
+// Single line comment                                                                                             
+
+#include <stdio.h>
+#include <string.h>
+
+#define MYNAME "Brandon Nguyen"
+
+int globalVar = 100;
+
+main() {
+
+    char firstLetter = 'B';
+
+    int age = 38;
+
+    long int superBigNumber = -327670000;
+
+    float piValue = 3.14159;
+
+    double reallyBigPi = 3.1459145914591459;
+
+    printf("\n"); // Skip a line                                                                                   
+
+    printf("This will print to screen\n\n"); // Two line breaks                                                    
+    
+}
