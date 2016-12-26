7311d3a3c398218b9f0c9ac2900d9a6f71b9b3a1
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-26 12:37:28 -0500

    Create HelloWorldV3.c

---
+++ KR Book/HelloWorldV3.c
@@ -0,0 +1,17 @@
+/* Hello World version 3*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    int i = 0;
+    char *str = "Hello World!\n";
+    
+    /* Print each character until reach '\0' */
+    while (str[i] != '\0')
+           printf("%c", str[i++]);
+           
+    return 0;
+    
+}
