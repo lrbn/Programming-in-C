5b5c26f6ae166b47627e861cb9688f3b79c0d980
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-26 22:51:56 -0500

    Update Pointers.c

--- KR Book/Pointers.c
+++ KR Book/Pointers.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+// A pointer is a variable that contains the address of a variable.
+// Every pointer points to a specific data type (Exeception: pointer to void)
+// The & operator gives the address of an object.
+// The urnary operator * is the indirection or deferencing operator; when applied to a pointer, it accesses the object the pointer points to.
+
+
 int main (void) {
 
     int x = 1, y = 2, z = 3;
     
     int *ip; /* ip is a pointer to int */
     
-    ip = &x; /* ip now points to x */
+    ip = &x; /* ip now points to x (Assigns the address of x to ip (ip points to x)) */
     
     y = *ip; /* y is now 1 */
     
