821298c4c9e705b4c040c1cc4bb9359b24c0ef0b
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-28 17:26:05 -0500

    Create CharacterCounting.c

---
+++ KR Book/CharacterCounting.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* count characters in input */
+
+main() {
+
+    long nc;
+
+    nc = 0;
+
+    while (getchar() != EOF) {
+
+        ++nc;
+
+    }
+
+    printf("%1d\n", nc);
+
+}
